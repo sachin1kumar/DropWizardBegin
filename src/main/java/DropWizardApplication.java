d850b6cbbceac6056955f65e583b9aca3e271e9e
Sachin <sachinkmr375@gmail.com>
2020-12-05 17:51:05 +0530

    workable drop wizard app

--- src/main/java/DropWizardApplication.java
+++ src/main/java/DropWizardApplication.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class DropWizardApplication {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class DropWizardApplication extends Application<DropWizardConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new DropWizardApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Hello Drop Wizard";
+    }
+
+    @Override
+    public void initialize(Bootstrap<DropWizardConfiguration> bootstrap) {
+
+    }
+
+    @Override
+    public void run(DropWizardConfiguration dropWizardConfiguration, Environment environment) throws Exception {
+        final DropWizardResource resource = new DropWizardResource(
+                dropWizardConfiguration.getTemplate(),
+                dropWizardConfiguration.getDefaultName());
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(dropWizardConfiguration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+
 }
--- src/main/java/DropWizardConfiguration.java
+++ src/main/java/DropWizardConfiguration.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class DropWizardConfiguration {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import javax.validation.constraints.NotEmpty;
+
+public class DropWizardConfiguration extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Drop Wizard";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
 }
